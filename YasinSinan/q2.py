c20988f7782256b63ef7577fe7aa5a4e66c31fe0
Yasin Sinan <yasinsinan1@gmail.com>
2025-01-21 22:29:32 +0100

    Yasinsinan q2.py

---
+++ YasinSinan/q2.py
@@ -0,0 +1,17 @@
+class rectangle:
+    def __init__(self,width, height ):
+        self.width=width
+        self.height=height
+    def perimeter(self):
+        return 2*(self.width+self.height)
+    def area(self):
+        return self.width*self.height
+rect = rectangle(5, 7)
+print("Area:", rect.area())
+
+rect= rectangle(5,7)
+print ("perimeter:", rect.perimeter())
+
+
+
+
