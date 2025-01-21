38ab989c5060564e7c1faf76315e75b3f3c8954a
Yasin Sinan <yasinsinan1@gmail.com>
2025-01-21 22:43:17 +0100

    Update q2.py

--- YasinSinan/q2.py
+++ YasinSinan/q2.py
@@ -1,16 +1,35 @@
-class rectangle:
-    def __init__(self,width, height ):
-        self.width=width
-        self.height=height
-    def perimeter(self):
-        return 2*(self.width+self.height)
-    def area(self):
-        return self.width*self.height
-rect = rectangle(5, 7)
-print("Area:", rect.area())
+class School:
+  def __init__(self, name, foundation_year):
+    self.name=name
+    self.foundation_year=foundation_year
+    self.students=[]
+    self.teachers=[]
 
-rect= rectangle(5,7)
-print ("perimeter:", rect.perimeter())
+    def add_new_student(self, student_name, student_class):
+        new_student = {'name': student_name,'class': student_class}
+        self.students.append(new_student)
+
+    def add_new_teacher(self, teacher_name, teacher_branch):
+        new_teacher={"name": teacher_name, "branch": teacher_branch}
+        self.teachers.append(new_teacher)
+
+    def view_student_list(self):
+        for i in self.students:
+            print(f"Name: {i['name']}, Class: {i['class']}")
+
+    def view_teacher_list(self):
+        for i in self.teachers:
+            print(f"Name: {i['name']}, Branch: {i['branch']}")
+
+
+school = School("Atatürk İlkokulu", 1984)
+school.add_new_student("Kemal", "5B")
+school.add_new_student("Ayşe", "6A")
+school.add_new_teacher("Yunus", "OOP")
+school.add_new_teacher("Yavuz", "Music")
+school.view_student_list()
+school.view_teacher_list()
+
 
 
 
