f8873d241e445cb72f6cc24c66745e4cbd2856e8
Yasin Sinan <yasinsinan1@gmail.com>
2025-01-24 21:22:06 +0100

    Update q3.py

--- YasinSinan/q3.py
+++ YasinSinan/q3.py
@@ -1,3 +1,10 @@
+# Question3: Create a "Shape" class. Under this class, create two subclasses, the "Rectangle" and "Square" classes.
+
+# Let the "shape" class have two properties: "width" and "height."
+# Let the "Rectangle" class inherit from the "Shape" class and add an additional "calculate_area()" method.
+# Let the "Square" class inherit from the "Shape" class and calculate the area of ​​the square using the same "area_calculate()" method.
+# Create a "Rectangle" and a "Square" instance, determine the width and height of each, and calculate the area of ​​each and print the results.
+
 class Shape:
     def __init__(self, width, height):
         self.width = width
